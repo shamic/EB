d8ce7f74dc7fc1f0ac03f6750b9fea3276e04781
Vary <Vary.Fan@walmart.com>
2018-04-10 17:47:26 +0800

    delete api & page navigation

--- controllers/book.js
+++ controllers/book.js
@@ -6,19 +6,18 @@ let Books = model.Books;
 const APIError = require('../rest').APIError;
 
 module.exports = {
-    'GET /api/books': async (ctx, next) => {
+    'POST /api/books': async (ctx, next) => {
         if (ctx.auth && ctx.auth.code != 0) {
             ctx.rest({
                 code: ctx.auth.code,
-                msg: ctx.auth.message,
-                data: null
+                msg: ctx.auth.message
             })
             return
         }
         var pageSize = parseInt(ctx.request.body.pageSize) || 20;
         var start = parseInt(ctx.request.body.start) || 0;
 
-        var resutl = await Books.findAndCountAll({offset: start, limit: pageSize}); //order: 'createdAt asc'
+        var resutl = await Books.findAndCountAll({offset: start, limit: pageSize, order: 'createdAt desc'}); //
         var books = resutl.rows
         books.map(element => {
             element.thumbnail_url = element.thumbnail_url || "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1522478341968&di=c60886853463d9f62c8c49d6fe270426&imgtype=0&src=http%3A%2F%2Fwww.memobook.com.tw%2Fsaved%2F13%2F131210%2F13121009462755%2Fthumb%2Fp8_GK5DYP_1.jpg";
@@ -30,19 +29,18 @@ module.exports = {
         // }
         if (books) {
             ctx.rest({
-                data: books
+                data: resutl
             });
         } else {
             throw new APIError('books:not_found', 'books not found.');
         }
     },
 
-    'POST /api/books': async (ctx, next) => {
+    'POST /api/addBook': async (ctx, next) => {
         if (ctx.auth && ctx.auth.code != 0) {
             ctx.rest({
                 code: ctx.auth.code,
-                msg: ctx.auth.message,
-                data: null
+                msg: ctx.auth.message
             })
             return
         }
@@ -84,6 +82,28 @@ module.exports = {
             });
         } else {
             throw new APIError('book created:fail', 'create book fail.');
+        }
+    },
+
+    'POST /api/deleteBook': async (ctx, next) => {
+        if (ctx.auth && ctx.auth.code != 0) {
+            ctx.rest({
+                code: ctx.auth.code,
+                msg: ctx.auth.message
+            })
+            return
+        }
+
+        var bookId = ctx.request.body.bookId;
+        var result = await Books.destroy({'where':{'id':bookId}});
+        
+        if (result > 0) {
+            logger.info('deleted book: ' + JSON.stringify(bookId));
+            ctx.rest({
+                data: bookId
+            });
+        } else {
+            throw new APIError('book deleted:fail', 'deleted book fail.');
         }
     },
 }
