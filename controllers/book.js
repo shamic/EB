f863848ed823736f10b59a2525069914b07a5ff6
Shamic <shamicfan@yeah.net>
2018-04-13 01:32:16 +0800

    delete unused codes

--- controllers/book.js
+++ controllers/book.js
@@ -25,7 +25,7 @@ module.exports = {
             element.thumbnail_url = element.thumbnail_url || "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1522478341968&di=c60886853463d9f62c8c49d6fe270426&imgtype=0&src=http%3A%2F%2Fwww.memobook.com.tw%2Fsaved%2F13%2F131210%2F13121009462755%2Fthumb%2Fp8_GK5DYP_1.jpg";
             element.rating = element.rating || "--";
         });
-        logger.info('find ${books.length} books:');
+        logger.info(`find ${books.length} books:`);
         // for (let book of books) {
         //     console.log(JSON.stringify(book));
         // }
